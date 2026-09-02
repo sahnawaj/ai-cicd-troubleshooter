17cf26d72d94c2fda04ce58ae3e07c860d0dba8a
sahnawaj <shanawaj.biswas@gmail.com>
2026-09-02 19:29:37 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,3 +1,10 @@
+
+//  app.py
+//  
+//
+//  Created by Aafiya Biswas on 02/09/26.
+//
+
 def calculate_total(price, quantity):
     print(f"Calculating total: price={price}, quantity={quantity}")
 
